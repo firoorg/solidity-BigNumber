4dc481e2829dbec1fc5969426ddf0b79e2ab9984
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-09-09 18:35:46 -0700

    Attempt to run on Solidity 0.5.x

--- contracts/BigNumber.sol
+++ contracts/BigNumber.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.20;
+pragma solidity >=0.4.20 <0.6;
 
 library BigNumber {
     
--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.17;
+pragma solidity >=0.4.20 <0.6;
 
 contract Migrations {
   address public owner;
